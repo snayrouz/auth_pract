0cd91bb0b138a79992ca074e1a9fe7dbea3d9857
snayrouz <samuel.nayrouz@gmail.com>
2018-07-24 11:57:29 -0600

    Add, config capybara  and setup first integration test for user creation

---
+++ auth_pract/test/integration/user_creation_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class UserCreationTest < ActionDispatch::IntegrationTest
+  include Capybara::DSL
+  test "a user can be created" do
+    #   assert true
+    # end
+  end
+end
