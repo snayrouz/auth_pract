ab2aeb22ca3276091af47b1b419c39c574bb0ed3
snayrouz <samuel.nayrouz@gmail.com>
2018-07-24 14:52:37 -0600

    Add user creation with passing tests

--- auth_pract/app/controllers/users_controller.rb
+++ auth_pract/app/controllers/users_controller.rb
@@ -4,6 +4,22 @@ class UsersController < ApplicationController
   end
 
   def create
-
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to @user
+    else
+      #rerender new view if validations fail
+    end
   end
+
+  def show
+    @user = current_user
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:username, :password)
+    end
 end
